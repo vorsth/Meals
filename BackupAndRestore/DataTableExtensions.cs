a5c004a179bcb760327a9f7e61e8dbc985358632
Hans Vorsteveld <hans.vorsteveld@johnzink.com>
2019-01-05 18:59:10 -0700

    feat(backup and restore): Add a simple backup + restore utility so making future schema changes is easier withouth losing data.  Backup to CSV files, make schema changes in database and CSV files, and then restore from CSV files

---
+++ BackupAndRestore/DataTableExtensions.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace BackupAndRestore
+{
+    public static class DataTableExtensions
+    {
+        private const char DELIMITER = '|';
+        public static void WriteCsv(this DataTable dataTable, Stream stream)
+        {
+            using(var sw = new StreamWriter(stream))
+            {
+                var columnNames = string.Join(DELIMITER, dataTable.Columns.Cast<DataColumn>().Select(x => x.ColumnName));
+                sw.WriteLine(columnNames);
+
+                var columnDataTypes = string.Join(DELIMITER, dataTable.Columns.Cast<DataColumn>().Select(x => x.DataType.AssemblyQualifiedName));
+                sw.WriteLine(columnDataTypes);
+
+                foreach(DataRow row in dataTable.Rows)
+                {
+                    sw.WriteLine(string.Join(DELIMITER, row.ItemArray.Select(x => x.ToString())));
+                }
+            }
+        }
+
+        public static DataTable ReadCsv(Stream stream)
+        {
+            var dt = new DataTable();
+            using(var sr = new StreamReader(stream))
+            {
+                var columns = sr.ReadLine().Split(DELIMITER);
+                var columnTypes = sr.ReadLine().Split(DELIMITER).Select( t => Type.GetType(t)).ToList();
+
+                for(var i = 0; i < columns.Count(); i++)
+                {
+                    dt.Columns.Add(columns[i], columnTypes[i]);
+                }
+
+                var line = sr.ReadLine().Split(DELIMITER);
+                while(line != null)
+                {
+                    var parsedValues = line.Select((x, i) => ConvertToType(x, columnTypes[i])).ToArray();
+
+                    dt.Rows.Add(parsedValues);
+                    line = sr.ReadLine()?.Split(DELIMITER);
+                }
+
+            }
+
+            return dt;
+        }
+
+        private static IConvertible ConvertToType(string val, Type t)
+        {
+            if(t == typeof(Int32))
+            {
+                if (string.IsNullOrEmpty(val))
+                {
+                    return 0;
+                }
+                else
+                {
+                    Int32.TryParse(val, out var result);
+                    return result;
+                }
+            }
+            else if(t == typeof(DateTime))
+            {
+                DateTime.TryParse(val, out var result);
+                return result;
+            }
+            else if(t == typeof(Single))
+            {
+                float.TryParse(val, out var result);
+                return result;
+            }
+            else if(t == typeof(string)){
+                return val;
+            }
+            throw new InvalidOperationException($"Could not convert the input to the type {t}");
+        }
+    }
+}
---
+++ BackupAndRestore/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using Npgsql;
+
+namespace BackupAndRestore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get connection string
+            var connString = Environment.GetEnvironmentVariable("MEALS_DB_CONN_STR");
+            var tableList = new List<string>()
+            {
+                "ingredient",
+                "unit",
+                "store",
+                "recipe",
+                "shoppinglist",
+                "ingredientstore",
+                "recipeingredient",
+                "shoppinglistrecipe",
+            };
+
+
+            Console.WriteLine("Options:\n" +
+                "B: Backup Database\n" +
+                "R: Restore Database\n");
+            Console.WriteLine("Enter Option: ");
+            var entry = Console.ReadLine();
+            if(entry.ToUpper() == "B")
+            {
+                foreach (var table in tableList)
+                {
+                    BackupTable(connString, table);
+                }
+            }
+            else if (entry.ToUpper() == "R")
+            {
+                foreach (var table in tableList)
+                {
+                    RestoreTable(connString, table);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Exiting.");
+            }
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        private static void BackupTable(string connString, string tableName)
+        {
+            Console.WriteLine($"Backing up table {tableName}...");
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                conn.Open();
+                using(var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = $"SELECT * FROM meals.{tableName}";
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        var dt = new DataTable();
+                        dt.TableName = tableName;
+                        dt.Load(reader);
+                        dt.WriteCsv(new FileStream($"{tableName}.csv", FileMode.Create));
+                    }
+                }
+            }
+        }
+
+        private static void RestoreTable(string connString, string tableName)
+        {
+            Console.WriteLine($"Restoring up table {tableName}...");
+
+            var dataTable = DataTableExtensions.ReadCsv(new FileStream($"{tableName}.csv", FileMode.Open));
+            var columnNames = string.Join(',', dataTable.Columns.Cast<DataColumn>().Select(x => x.ColumnName));
+            var valuesParamsList = new List<string>();
+            for(var i = 0; i < dataTable.Columns.Count; i++)
+            {
+                valuesParamsList.Add($"@value{i}");
+            }
+            var valueParams = string.Join(',', valuesParamsList);
+            
+
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                conn.Open();
+                var totalRows = 0;
+                foreach(DataRow row in dataTable.Rows)
+                {
+                    using(var cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = $"insert into meals.{tableName} ({columnNames}) VALUES ({valueParams})";
+                        for (var i = 0; i < row.ItemArray.Count(); i++)
+                        {
+                            cmd.Parameters.Add(new NpgsqlParameter($"@value{i}", row.ItemArray[i]));
+                        }
+                        totalRows += cmd.ExecuteNonQuery();
+                    }
+                }
+                Console.WriteLine($"Wrote {totalRows} rows to meals.{tableName}");
+            }
+        }
+    }
+}

fed8836727607fe20ade5a84676b1b71e7e7277f
Hans Vorsteveld <hans.vorsteveld@johnzink.com>
2019-01-06 19:08:19 -0700

    feat(shop): Add a link on the Shopping Lists page that generates a list of the ingredients needed for that list.

---
+++ Meals/ViewModels/ShopVM.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Meals.Models;
+
+namespace Meals.ViewModels
+{
+    public class ShopVM
+    {
+        public ShoppingList ShoppingList { get; set; }
+        public IEnumerable<ShoppingListIngredient> Ingredients { get; set; }
+
+        public ShopVM(ShoppingList list, IEnumerable<ShoppingListIngredient> ingredients)
+        {
+            this.ShoppingList = list;
+            this.Ingredients = ingredients;
+        }
+    }
+}
---
+++ Meals/ViewModels/ShoppingListIngredient.cs
@@ -0,0 +1,23 @@
+﻿using Meals.Models;
+
+namespace Meals.ViewModels
+{
+    public class ShoppingListIngredient
+    {
+        public Ingredient Ingredient { get; set; }
+        public int Quantity { get; set; }
+        public Unit Unit { get; set; }
+
+        public ShoppingListIngredient(Ingredient i, int quantity, Unit u)
+        {
+            this.Ingredient = i;
+            this.Quantity = quantity;
+            this.Unit = u;
+        }
+
+        public override string ToString()
+        {
+            return $"{Quantity} {Unit.Name} {Ingredient.Name}";
+        }
+    }
+}
